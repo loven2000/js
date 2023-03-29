47ad2d94821b47891119bc61bc410c334fb3dbf2
loven <loven2000@naver.com>
2023-03-29 11:09:56 +0900

    js 조건문 1,2

--- momentum/script.js
+++ momentum/script.js
@@ -1,10 +1,19 @@
-// js return 강좌
-const age = 96;
-function calculateKrAge(ageOfForeigner){
-     ageOfForeigner + 2;
-     return 'hello';
-};
+// js Conditionals 조건문
+// const age = prompt('How old are you?');
+const age = parseInt( prompt('How old are you?'));
 
-const krAge = calculateKrAge(age);
+// console.log(typeof age);
+// parseInt() 문자를 숫자로 바꾸어 주는 함수.
+// console.log(age, parseInt(age));
 
-console.log(krAge);
+// console.log(age);
+
+// console.log(isNaN(age)); // isNaN 숫자가 아니냐? Not a Number, boolean(true, false)으로 답변 
+
+if(isNaN(age)){
+    /// condition === true
+    console.log("please write a number")
+}else{
+    /// condition === false
+    console.log("thank you for writing your age.")
+}
