fee815312415e8ee4b1e1efd43176cc587686f16
loven <loven2000@naver.com>
2023-03-29 11:34:29 +0900

    조건문 3번째 강의

--- momentum/script.js
+++ momentum/script.js
@@ -10,10 +10,15 @@ const age = parseInt( prompt('How old are you?'));
 
 // console.log(isNaN(age)); // isNaN 숫자가 아니냐? Not a Number, boolean(true, false)으로 답변 
 
-if(isNaN(age)){
+if(isNaN(age) || age < 0){
     /// condition === true
-    console.log("please write a number")
-}else{
+    console.log("please write a real positive number.")
+}else if(age < 18){
     /// condition === false
-    console.log("thank you for writing your age.")
+    console.log("Your are too young.")
+}else if(age >= 18 && age <= 50){
+    // 조건문의 경우의 수 - || or (한쪽만 참이면 됨) && and (둘다 참이어야 함)
+    console.log("You can drink but drink too much is not good for your health")
+}else{
+    console.log("You can drink.")
 }
