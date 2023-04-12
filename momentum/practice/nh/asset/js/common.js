40a00ddeeb99310020ae6e472fbe88f91a639f05
loven <loven2000@naver.com>
2023-04-12 15:43:06 +0900

    23.04.12

--- momentum/practice/nh/asset/js/common.js
+++ momentum/practice/nh/asset/js/common.js
@@ -24,8 +24,67 @@ mainMenu1.forEach(function (mainMenu1) {
         onElement.classList.remove("on");
       }
     });
+   
+  });
+
+});
+
+//gnb 열기, 닫기
+$(document).ready(function(){
+  $(".btn-menu").click(function(){
+    $(".gnb").addClass("on");
+    $("body").addClass("overflow-hidden")
+  });
+
+  $(".btn-close").click(function(){
+    $(".gnb").removeClass("on");
+    $("body").removeClass("overflow-hidden")
   });
 });
+
+// 메인 스크롤 헤더 
+    const visual = document.querySelector('.visual');
+		const scrollHeader = document.querySelector('.scroll-header');
+		
+		window.addEventListener('scroll', () => {
+			if (window.scrollY > visual.scrollTop) {
+				scrollHeader.style.display = 'block';
+			} else {
+				scrollHeader.style.display = 'none';
+			}
+		});
+
+// 메인 tab 공지사항
+const tabs = document.querySelectorAll('notice__tab-title li');
+		const tabContents = document.querySelectorAll('.notice__list');
+
+		tabs.forEach(tab => {
+			tab.addEventListener('click', () => {
+				// Remove active class from all tabs and contents
+				tabs.forEach(t => t.classList.remove('active'));
+				tabContents.forEach(tc => tc.classList.remove('active'));
+
+				// Add active class to clicked tab and corresponding content
+				tab.classList.add('active');
+				const tabId = tab.dataset.tab;
+				const tabContent = document.querySelector(`.notice__list[data-tab="${tabId}"]`);
+				tabContent.classList.add('active');
+			});
+		});
+// const menuTitles = document.querySelectorAll(".menu-tap__title > li");
+// const menuSwipers = document.querySelectorAll(".menuSwiper .swiper-slide");
+
+// menuTitles.forEach(function (title, index) {
+//   title.addEventListener("click", function () {
+//     menuSwipers.forEach(function (swiper, swiperIndex) {
+//       if (index === swiperIndex) {
+//         swiper.style.display = "block";
+//       } else {
+//         swiper.style.display = "none";
+//       }
+//     });
+//   });
+// });
 
   //비율유지 박스
 //   $(window).resize(function(){
--- momentum/script.js
+++ momentum/script.js
@@ -1,19 +1,4 @@
-// Document Object
-// document.title = "Hello! from JS"
+const amIFat = null;
+let something;
 
-// const title = document.getElementById("title");
-
-// console.dir(title);
-
-// title.innerText = "Got you!!"
-
-// const hellos =document.getElementsByClassName("hello");
-
-// console.log(hellos);
-
-// const title = document.getElementsByTagName("h1");
-// console.log(title);
-
-const title = document.querySelector(".hello h1");
-
-console.log(title);
+console.log(something, amIFat);
